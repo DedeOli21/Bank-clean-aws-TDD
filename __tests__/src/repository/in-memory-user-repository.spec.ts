3c6687daf778f9c1bd4b5603f18b354faa6ce312
David <dedepco@gmail.com>
2022-06-02 23:30:09 -0300

    test: return all users in repository

--- __tests__/src/repository/in-memory-user-repository.spec.ts
+++ __tests__/src/repository/in-memory-user-repository.spec.ts
@@ -19,4 +19,19 @@ describe('In memory User repository', () => {
     const user = await userRepo.findUserByUserId('userId')
     expect(user.userId).toEqual('userId')
   })
+
+  test('should return all users', async () => {
+    const users: UserData[] = [{
+      userId: 'userId',
+      email: 'email',
+      password: 'password'
+    }, {
+      userId: 'userId2',
+      email: 'email2',
+      password: 'password2'
+    }]
+    const sut = new InMemoryUserRepository(users)
+    const result = await sut.findAllUsers()
+    expect((await result).length).toBe(2)
+  })
 })
