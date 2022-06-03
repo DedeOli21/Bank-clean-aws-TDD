74af710dd4446916e98e4e27cf94236a611f64b9
David <dedepco@gmail.com>
2022-06-02 23:19:04 -0300

    chore: add jest config and install dotenv

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,18 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
-  clearMocks: true,
-  collectCoverage: true,
+  // preset: '@shelf/jest-mongodb',
+  roots: ['<rootDir>/__tests__'],
   coverageDirectory: 'coverage',
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!**/__tests__/**',
+    '!**/config/**'
+  ],
   transform: {
     '.+\\.ts$': 'ts-jest'
-  }
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@test/(.*)$': '<rootDir>/__tests__/$1'
+  },
+  setupFiles: ['dotenv/config']
 }
