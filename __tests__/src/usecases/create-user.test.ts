915699fafe50441c102de362d5cd11bdcc5a1e7b
David <dedepco@gmail.com>
2022-06-02 23:19:35 -0300

    feat: create-user, example

---
+++ __tests__/src/usecases/create-user.test.ts
@@ -0,0 +1,8 @@
+import { UserData } from '../../../src/usecases/create-user/user-data'
+
+describe('Create user', () => {
+  test('Return 400 if UserId is not provided', async () => {
+    const users: UserData[] = []
+    return users
+  })
+})
