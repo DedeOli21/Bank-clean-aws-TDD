c3ae2bd5076792efa2c072e90763f290166272b6
David <dedepco@gmail.com>
2022-06-06 21:47:14 -0300

    test: create function perfom to add User

--- __tests__/src/usecases/create-user.test.ts
+++ __tests__/src/usecases/create-user.test.ts
@@ -1,8 +1,19 @@
+import { UserRepository } from '../../../src/usecases/create-user/ports/user-repository'
+import { InMemoryUserRepository } from '../../../src/usecases/create-user/repository/in-memory-user-repository'
 import { UserData } from '../../../src/usecases/create-user/user-data'
+import { CreateUser } from '../../../src/usecases/create-user/create-user'
+import { User } from '../../../src/usecases/create-user/user'
 
 describe('Create user', () => {
-  test('Return 400 if UserId is not provided', async () => {
+  test('Should create user with valid data', async () => {
     const users: UserData[] = []
-    return users
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const usecase: CreateUser = new CreateUser(repo)
+    const userId = 'any_user_id'
+    const email = 'any@email.com'
+    const password = 'valid_password'
+    const user = User.create({ userId, email, password }) as User
+    const response = await usecase.perform(user)
+    expect(response.userId).toBe(userId)
   })
 })
