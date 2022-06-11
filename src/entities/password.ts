52f9462418944674df9a9999cd0cc08418a59b15
David <dedepco@gmail.com>
2022-06-10 23:02:36 -0300

    feat: create entite of Password with validate

---
+++ src/entities/password.ts
@@ -0,0 +1,26 @@
+import { Either, left, right } from '@/shared'
+import { InvalidPasswordError } from './errors'
+
+export class Password {
+  public readonly value: string
+
+  private constructor (password: string) {
+    this.value = password
+  }
+
+  public static create (password: string): Either<InvalidPasswordError, Password> {
+    if (Password.validate(password)) {
+      return right(new Password(password))
+    }
+
+    return left(new InvalidPasswordError(password))
+  }
+
+  static validate (password: string): boolean {
+    if (!password) {
+      return false
+    }
+
+    return true
+  }
+}
