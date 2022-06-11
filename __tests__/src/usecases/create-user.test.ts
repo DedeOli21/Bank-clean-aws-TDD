67aa4753a54dd9163083bcbdbf4233452d8a46f3
David <dedepco@gmail.com>
2022-06-10 23:07:00 -0300

    test: add create-user-test

--- __tests__/src/usecases/create-user.test.ts
+++ __tests__/src/usecases/create-user.test.ts
@@ -1,8 +1,8 @@
-import { UserRepository } from '../../../src/usecases/create-user/ports/user-repository'
-import { InMemoryUserRepository } from '../../../src/usecases/create-user/repository/in-memory-user-repository'
-import { UserData } from '../../../src/usecases/create-user/user-data'
-import { CreateUser } from '../../../src/usecases/create-user/create-user'
-import { User } from '../../../src/usecases/create-user/user'
+import { UserRepository } from '@/usecases/create-user/ports/user-repository'
+import { InMemoryUserRepository } from '@/usecases/create-user/repository/in-memory-user-repository'
+import { UserData } from '@/usecases/create-user/user-data'
+import { CreateUser } from '@/usecases/create-user/create-user'
+import { User } from '@/entities'
 
 describe('Create user', () => {
   test('Should create user with valid data', async () => {
@@ -12,8 +12,20 @@ describe('Create user', () => {
     const userId = 'any_user_id'
     const email = 'any@email.com'
     const password = 'valid_password'
-    const user = User.create({ userId, email, password }) as User
+    const user = User.create({ userId, email, password }).value as User
     const response = await usecase.perform(user)
     expect(response.userId).toBe(userId)
+  })
+
+  test('Should not create user without UserId data', async () => {
+    const users: UserData[] = []
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const usecase: CreateUser = new CreateUser(repo)
+    const userId = null
+    const email = 'any@email.com'
+    const password = 'valid_password'
+    const user = User.create({ userId, email, password }).value as User
+    const response = await usecase.perform(user)
+    expect(response).toBe('UserId is required')
   })
 })
